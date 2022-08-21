2e2f704ba035f3fee6ffe0a5d39f039e99ea542f
DariaChervonooka <111647599+DariaChervonooka@users.noreply.github.com>
2022-08-21 16:17:41 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,58 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int data = 255;
+float payload = 6.153;
+bool flag = true;
+char ch(35);
+
+int main()
+{
+    cout<<"data value is :"<<data<<endl;
+    cout<<"payload value is :"<<payload<<endl;
+    std::cout << std::boolalpha;
+    cout<<"flag value is :"<<flag<<endl;
+    cout<<"ch value is :"<<ch<<endl<<endl;
+    
+    
+     int a = 1;
+    a = 10;
+    cout<<a<<endl;
+    a += 100;
+    cout<<a<<endl;
+    a -=20;
+    cout<<a<<endl;
+    a *=3;
+    cout<<a<<endl;
+    a /=2;
+    cout<<a<<endl;
+    cout<<endl;
+    
+    
+    float g = 0;
+     float h = 1;;
+     double q = 1.5426;
+     
+     
+     
+     int value = 3;
+    value = value + 1;
+     cout << value << endl; 
+     cout<<endl;
+     
+     int userID = 0x12;
+    userID += 5;
+    cout << userID << endl;
+    
+
+    return 0;
+}
